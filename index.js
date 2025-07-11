d58f56de658dac2daeab0c1bf4489d8f773c7739
harshkumar7646 <sirihey0047@gmail.com>
2025-07-11 20:11:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
+const { connectDB } = require("./connection.js");
+const url = require("./model.js");
+const express = require("express");
+const path = require("path");
+const app = express();
+const router = require("./routes.js");
+const { getOriginalUrl } = require("./controllers.js");
+const port = 8000;
 
-const {connectDB}=require("./connection.js");
-const url=require("./model.js")
-const express=require("express");
-const path=require("path");
-const app=express();
-const router=require("./routes.js");
-const port=8000;
-app.listen(port,()=>{
-    console.log(`server.listen at ${port}`)
-})
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.set("views",path.resolve("./views"));
-app.use("/api/users",router);
+app.set("views", path.resolve("./views"));
+
+// Root route
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+// API routes
+app.use("/api/users", router);
+
+// Short URL redirect route (at root level)
+app.get("/:cust_url", getOriginalUrl);
+
+app.listen(port, () => {
+    console.log(`server listening at ${port}`);
+});
+
 connectDB();
