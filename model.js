6b8d6926c2f04fad492d267cbc5a2195232e8537
harshkumar7646 <sirihey0047@gmail.com>
2025-07-11 19:53:42 +0530

    Update model.js

--- model.js
+++ model.js
@@ -1,16 +1,15 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const urlSchema= new mongoose.Schema({
-    cust_url:{
-        type:String,
-        require:true
+const urlSchema = new mongoose.Schema({
+    cust_url: {
+        type: String,
+        required: true
     },
-    original_url:{
-        type:String,
-        required:true
-        
+    original_url: {
+        type: String,
+        required: true
     }
-})
+});
 
-const url=mongoose.model("urlData",urlSchema);
-module.exports=url;
+const url = mongoose.model("urlData", urlSchema);
+module.exports = url;
