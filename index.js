3775ef26c60f445e26ac6a4e0892acc59c28985d
harshkumar7646 <sirihey0047@gmail.com>
2025-07-11 18:08:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require("dotenv").config(); 
+
 const {connectDB}=require("./connection.js");
 const url=require("./model.js")
 const express=require("express");
 const path=require("path");
 const app=express();
 const router=require("./routes.js");
-const port=process.env.PORT;
+const port=8000;
 app.listen(port,()=>{
     console.log(`server.listen at ${port}`)
 })
