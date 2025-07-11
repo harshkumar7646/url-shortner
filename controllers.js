b3c15c1f5ec039ca8845b2a3ee1406e8b354e570
harshkumar7646 <sirihey0047@gmail.com>
2025-07-11 19:51:48 +0530

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -1,52 +1,53 @@
 require("dotenv").config();
-const url=require("./model.js");
+const url = require("./model.js");
 
-const getOriginalUrl=async (req,res)=>{
-    try{
-        const {cust_url}=req.params;
+const getOriginalUrl = async (req, res) => {
+    try {
+        const { cust_url } = req.params;
         console.log(cust_url);
-        const rec=await url.findOne({cust_url:cust_url});
+        const rec = await url.findOne({ cust_url: cust_url });
         console.log(rec);
-        if (!rec){
-            return res.status(404).json({"message":"not found"});
+        if (!rec) {
+            return res.status(404).json({ "message": "not found" });
         }
         return res.redirect(rec.original_url);
     }
-    catch(error){
-        return res.status(400).end();
+    catch (error) {
+        console.log("Error in getOriginalUrl:", error);
+        return res.status(400).json({ "message": "Bad request" });
     }
 }
 
-const insertdata=async (req,res)=>{
-    try{
-        const body=req.body;
+const insertdata = async (req, res) => {
+    try {
+        const body = req.body;
         await url.create({
-            cust_url:body.cust_url,
-            original_url:body.original_url
-        })
+            cust_url: body.cust_url,
+            original_url: body.original_url
+        });
         
         const baseUrl = process.env.BASE_URL || "";
         res.render("home", {
             GENurl: body.cust_url,
-            SHORTLINK: ${baseUrl}/${body.cust_url}
+            SHORTLINK: ${baseUrl}/api/users/${body.cust_url}
         });
     }
-    catch(error){
-        res.json({"status":"error in post"}).end();
-        console.log(error);
+    catch (error) {
+        console.log("Error in insertdata:", error);
+        res.status(400).json({ "status": "error in post" });
     }
 }
 
-const getHistory=async (req,res)=>{
-    try{
-        const ress=await url.find({});
+const getHistory = async (req, res) => {
+    try {
+        const ress = await url.find({});
         console.log(ress);
-        res.render("home",{response:ress});
+        res.render("home", { response: ress });
     }
-    catch(error){
+    catch (error) {
         console.log(error);
-        res.status(400).json({"message":"error occured in history"})
+        res.status(400).json({ "message": "error occurred in history" });
     }
 }
 
-module.exports={getOriginalUrl,insertdata,getHistory};
+module.exports = { getOriginalUrl, insertdata, getHistory };
