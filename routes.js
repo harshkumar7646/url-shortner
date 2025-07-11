d0052c247826665d8b38888e4f7d8fb0189e1def
harshkumar7646 <sirihey0047@gmail.com>
2025-07-11 20:11:51 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,11 +1,12 @@
-const express=require("express");
-const router=express.Router();
-const {getOriginalUrl,insertdata,getHistory}=require("./controllers")
-router.get("/history",getHistory)
-router.get("/",(req,res)=>{
-    res.render("home");
-})  
-router.get("/:cust_url",getOriginalUrl)
-router.post("/create",insertdata)
 
-module.exports=router;
+const express = require("express");
+const router = express.Router();
+const { insertdata, getHistory } = require("./controllers.js");
+
+router.get("/history", getHistory);
+router.get("/", (req, res) => {
+    res.render("home");
+});
+router.post("/create", insertdata);
+
+module.exports = router;
