7ddc2f549d1d81eb01b3852983e9f49ebc4510dd
harshkumar7646 <sirihey0047@gmail.com>
2025-07-11 19:54:19 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,11 +1,12 @@
-const mongoose=require("mongoose");
-async function connectDB(){
-    try{
-        mongoose.connect(process.env.MONGO_URL)
+const mongoose = require("mongoose");
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("DATABASE CONNECTED");
-    }catch(error){
-        console.log("error:",error);
+    } catch (error) {
+        console.log("Database connection error:", error);
     }
 }
 
-module.exports={connectDB};
+module.exports = { connectDB };
