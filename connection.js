cd8b33691eca119fcfd84a788e65f7cd29492feb
harshkumar7646 <sirihey0047@gmail.com>
2025-07-11 18:20:12 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 async function connectDB(){
     try{
-        mongoose.connect("mongodb+srv://sirihey0047:jB2cXi8qreRQTQSI@cluster0.gg1m5yi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        mongoose.connect(process.env.MONGO_URL);
         console.log("DATABASE CONNECTED");
     }catch(error){
         console.log("error:",error);
